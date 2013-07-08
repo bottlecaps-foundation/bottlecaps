4f73f3839133ef8583039dd2471397e61cf613b9
bottlecaps-foundation <cookiekiller420@gmail.com>
2013-07-08 16:27:44 -0500

    Updated Main.cpp Checkpoint Notifications

--- src/main.cpp
+++ src/main.cpp
@@ -2787,7 +2787,11 @@ string GetWarnings(string strFor)
 
     // ppcoin: should not enter safe mode for longer invalid chain
     // ppcoin: if sync-checkpoint is too old do not enter safe mode
-
+    if (Checkpoints::IsSyncCheckpointTooOld(60 * 60 * 24 * 10) !fTestNet && !IsInitialBlockDownload())
+    {
+    	nPriority = 100;
+    	strStatusBar = "WARNING: Checkpoint is too old. Redownload Blockchain. If warning perpist contact the Development Team";
+    }
 
     // ppcoin: if detected invalid checkpoint enter safe mode
     if (Checkpoints::hashInvalidCheckpoint != 0)
