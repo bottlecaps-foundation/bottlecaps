a21b594615cf79a7587f6d50e31a188ee05ebb4b
bottlecaps-foundation <cookiekiller420@gmail.com>
2013-08-10 03:51:18 -0500

    Disconnect all clients previous to 1.4.1 at block 77500

--- src/version.h
+++ src/version.h
@@ -5,6 +5,7 @@
 #define BITCOIN_VERSION_H
 
 #include "clientversion.h"
+#include "main.h"
 #include <string>
 
 //
@@ -25,10 +26,20 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60006;
+
+static const int PROTOCOL_VERSION = 70000;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+
+if (nheight > 77500)
+{
+static const int MIN_PROTO_VERSION = 60008;
+}
+
+else
+{
+static const int MIN_PROTO_VERSION = 70000;
+}
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -36,7 +47,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
-static const int NOBLKS_VERSION_END = 60004;
+static const int NOBLKS_VERSION_END = 60007;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
@@ -45,8 +56,9 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 #define DISPLAY_VERSION_MAJOR       1
-#define DISPLAY_VERSION_MINOR       2
+#define DISPLAY_VERSION_MINOR       4
 #define DISPLAY_VERSION_REVISION    1
 #define DISPLAY_VERSION_BUILD       0
 
 #endif
+
