94c2329c6ff18ab9cd15e6702256d60f35788bae
bottlecaps-foundation <cookiekiller420@gmail.com>
2013-08-15 05:12:07 -0500

    Protocol Changes 1.4.1

--- src/version.h
+++ src/version.h
@@ -25,14 +25,14 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60008;
+static const int PROTOCOL_VERSION = 70000;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+static const int MIN_PROTO_VERSION = 60008;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 70000;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
@@ -46,7 +46,7 @@ static const int MEMPOOL_GD_VERSION = 60002;
 
 #define DISPLAY_VERSION_MAJOR       1
 #define DISPLAY_VERSION_MINOR       4
-#define DISPLAY_VERSION_REVISION    0
+#define DISPLAY_VERSION_REVISION    1
 #define DISPLAY_VERSION_BUILD       0
 
 #endif
