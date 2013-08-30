4651fb6e7a592f30d79cd32f7a5e826cbf94f48f
bottlecaps-foundation <cookiekiller420@gmail.com>
2013-08-30 12:56:33 -0500

    Timestamp Fix

--- src/main.h
+++ src/main.h
@@ -42,7 +42,7 @@ static const unsigned int PROTOCOL_SWITCH_TIME = 1371686400; // 20 Jun 2013 00:0
 
 static const unsigned int REWARD_SWITCH_TIME = 1369432800; // 25 May 2013 00:00:00
 
-static const unsigned int REWARD_FIX_SWITCH_TIME = 1378425600; // 7 SEP 2013 00:00:00 GMT
+static const unsigned int REWARD_FIX_SWITCH_TIME = 1378512000; // 7 SEP 2013 00:00:00 GMT
 
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
