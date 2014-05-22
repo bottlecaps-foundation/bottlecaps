4a83be522d913c884a82655c16899af813189f0f
Tranz5 <Tranz5@users.noreply.github.com>
2014-05-22 19:30:50 -0400

    Use GetPoWMHashPS and GetPoSKernelPS in getmininginfo

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -74,6 +74,8 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("currentblocktx",(uint64_t)nLastBlockTx));
     obj.push_back(Pair("difficulty",    (double)GetDifficulty()));
     obj.push_back(Pair("errors",        GetWarnings("statusbar")));
+    obj.push_back(Pair("netmhashps",     GetPoWMHashPS()));
+    obj.push_back(Pair("netstakeweight", GetPoSKernelPS()));
     obj.push_back(Pair("generate",      GetBoolArg("-gen")));
     obj.push_back(Pair("genproclimit",  (int)GetArg("-genproclimit", -1)));
     obj.push_back(Pair("hashespersec",  gethashespersec(params, false)));
