1279707831495d832265bb6ddd0174f2aceadfd6
Tranz5 <Tranz5@users.noreply.github.com>
2014-05-23 23:23:29 -0400

    Added another DNS Seed

--- src/net.cpp
+++ src/net.cpp
@@ -1152,6 +1152,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
+    {"capseed", "seed.bottlecaps.org"},
     {"btcrypt", "cap.nodes.btcrypt.net"},
 };
 
