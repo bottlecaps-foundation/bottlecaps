3b5700b6c46a37ee110aba166837b3b23d2ffe43
Tranz5 <Tranz5@users.noreply.github.com>
2015-06-24 22:40:12 -0400

    Update version.cpp
    bump

--- src/version.cpp
+++ src/version.cpp
@@ -36,8 +36,8 @@ const std::string CLIENT_NAME("Satoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "9f71e235"
-#    define GIT_COMMIT_DAdTE "Sun Jan 18 14:24:00 2015"
+#    define GIT_COMMIT_ID "418187240"
+#    define GIT_COMMIT_DAdTE "Wed Jun 24 22:12:00 2015"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
