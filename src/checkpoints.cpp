24340256d50091dc196dc57a6bbf466172bb9cb9
Tranz5 <Tranz5@users.noreply.github.com>
2015-06-24 22:35:13 -0400

    Update checkpoints.cpp
    remove checkpoint warning.

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -361,6 +361,8 @@ namespace Checkpoints
     // Is the sync-checkpoint too old?
     bool IsSyncCheckpointTooOld(unsigned int nSeconds)
     {
+        // Tranz for now just return false
+        return false;
         LOCK(cs_hashSyncCheckpoint);
         // sync-checkpoint should always be accepted block
         assert(mapBlockIndex.count(hashSyncCheckpoint));
